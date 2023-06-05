090ec68c0055d774a68ceb5b5f897ce229e42410
Sourabh10122002 <souravrawat444@gmail.com>
2023-06-05 15:05:18 +0530

    video 10

---
+++ src/components/TextForm.js
@@ -0,0 +1,67 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+  const handleUpClick = ()=> {
+    // console.log("Uppercase was clicked" + text);
+    let newText = text.toUpperCase();
+    setText(newText)
+  }
+
+  const handleLoClick = () =>  {
+    let newText = text.toLowerCase();
+    setText(newText)
+  }
+
+  const handleClearClick = ()=> {
+    let newText = '';
+    setText(newText)
+  }
+
+  const handleCopy = ()=> {
+    let newText = document.getElementById("myBox");
+    newText.select();
+    navigator.clipboard.writeText(newText.value);
+  }
+
+  const handleExtraSpaces = ()=> {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  }
+
+  const handleOnChange = (event)=> {
+    // console.log("On change");
+    setText(event.target.value);
+  }
+
+  const [text, setText] = useState('');
+
+  return (
+    <>
+    <div className="container">
+        <h1>{props.heading}</h1>
+  <div className="mb-3">
+    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+  </div>
+
+  <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+
+  <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+
+  <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
+
+  <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+
+  <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+
+  </div>
+
+  <div className="container my-3">
+    <h2>Your text summary</h2>
+    <p>{text.split(" ").length} words and {text.length} characters</p>
+    <p>{0.008 * text.split(" ").length} Minutes to Read</p>
+    <h2>Preview</h2>
+    <p>{text}</p>
+  </div>
+  </>
+  )
+}
